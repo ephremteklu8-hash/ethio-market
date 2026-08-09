2cf5eaab9021bc00f2169789cfbaa812c9f9efc1
ephremteklu8-hash <ephremteklu8@gmail.com>
2026-08-09 16:36:51 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,23 +1,22 @@
 plugins {
-id("com.android.application")
-id("org.jetbrains.kotlin.android")
-id("com.google.gms.google-services")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
-namespace = "com.ethiomarket.app"
-compileSdk = 35
+    namespace = "com.ethiomarket.app"
+    compileSdk = 35
 
-defaultConfig {
-    applicationId = "com.ethiomarket.app"
-    minSdk = 23
-    targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
-}
-
+    defaultConfig {
+        applicationId = "com.ethiomarket.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
 
 dependencies {
-implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
 }
